2412e98db5937253d40f7a8933d137cc6af2e7d1
SOFTWARESAPSECURITAS <149879548+SOFTWARESAPSECURITAS@users.noreply.github.com>
2025-06-20 12:58:26 +0530

    add mail address

--- php/contact.php
+++ php/contact.php
@@ -36,7 +36,8 @@ $mail->Port = 465; // TCP port to connect to
 
         // Sender and recipient settings
         $mail->setFrom($email, $name);                 // From the user's email
-        $mail->addAddress('office@firststepadvisors.in', 'Shivani');  // Your email to receive the message
+        $mail->addAddress('office@firststepadvisors.in', 'Shivani');
+        $mail->addAddress('info@firststepadvisors.in', 'Shivani');  // Your email to receive the message
         $mail->addReplyTo($email, $name);              // Reply to user's email
 
         // Email content
